2e06fa0c03bfcf653d538b8d4f5f99fb95d07dc3
Zzzen <callmetsing@gmail.com>
2022-07-02 23:36:56 +0800

    init commit

---
+++ build.mjs
@@ -0,0 +1,28 @@
+#!/usr/bin/env zx
+
+// import "zx/globals";
+import * as fs from "fs";
+
+const tsconfigPath = "./pyright/packages/pyright-internal/tsconfig.json"
+const packagePath = "./pyright/packages/pyright-internal/package.json"
+
+const tsconfig = require(tsconfigPath);
+const packageJson = require(packagePath)
+
+tsconfig.compilerOptions = Object.assign(tsconfig.compilerOptions, {
+  declaration: true,
+  preserveConstEnums: true,
+});
+
+fs.writeFileSync(tsconfigPath, JSON.stringify(tsconfig, undefined, 2))
+
+cd('pyright/packages/pyright-internal')
+await $`npm ci`
+await $`npm run build`
+await $`mv out/src ../../../dist`
+cd('../../../')
+
+packageJson.name = "@zzzen/pyright-internal"
+packageJson.version = "1.2.0-beta"
+
+fs.writeFileSync('package.json', JSON.stringify(packageJson, undefined, 2))
