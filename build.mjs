62f75454599c8bd634774a843bd2a41e7f7b9210
Heyward Fann <fannheyward@users.noreply.github.com>
2024-02-18 14:36:14 +0800

    fix: cd into the pyright folder

--- build.mjs
+++ build.mjs
@@ -16,6 +16,7 @@ tsconfig.compilerOptions = Object.assign(tsconfig.compilerOptions, {
 
 fs.writeFileSync(tsconfigPath, JSON.stringify(tsconfig, undefined, 2));
 
+cd("pyright");
 await $`npm ci`;
 cd("pyright/packages/pyright-internal");
 await $`npm ci`;
